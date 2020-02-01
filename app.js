4b798139aff1b3a0a0ca1820fae018eec2da2eb1
NuwanJ <nuwanjaliyagoda@gmail.com>
2020-02-01 19:19:16 +0530

    done

--- app.js
+++ app.js
@@ -18,6 +18,116 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.listen(9090);
 
+//-- API Web Server Template----------------------------------------------------
+
+app.route('/template').post(function(req,res){
+   if(1){
+
+      res.writeHead(400,{'Content-Type' : 'application/json', 'Access-Control-Allow-Origin':'*'});
+      res.end(JSON.stringify({'message':'Mobile no is not set', 'developerMessage':'User creation failed because the mobile no is not set'}));
+   }else{
+      // Bad Request
+      res.writeHead(400,{'Content-Type' : 'application/json', 'Access-Control-Allow-Origin':'*'});
+      res.end(JSON.stringify({'message':'Bad Request', 'developerMessage':'Bad Reequest'}));
+   }
+
+});
+
+// issue#2
+app.route('/api').get(function(req,res){
+   // Success
+   res.writeHead(200,{'Content-Type' : 'application/json', 'Access-Control-Allow-Origin':'*'});
+   res.end(JSON.stringify({'message':'Success', 'developerMessage':'Success'}));
+});
+
+// issue#3
+app.route('/api/users').post(function(req,res){
+
+   const nic = req.body.nic;
+   const mobile = req.body.mobile;
+   const password = req.body.password;
+
+   if(!mobile) {
+      res.writeHead(400,{'Content-Type' : 'application/json', 'Access-Control-Allow-Origin':'*'});
+      res.end(JSON.stringify({'message':'Mobile no is not set', 'developerMessage':'User creation failed because the mobile no is not set'}));
+      return;
+   }
+   else if(!nic) {
+      res.writeHead(400,{'Content-Type' : 'application/json', 'Access-Control-Allow-Origin':'*'});
+      res.end(JSON.stringify({'message':'Nic no is not set', 'developerMessage':'User creation failed because the nic no is not set'}));
+      return;
+   }
+
+   // Password check
+   if(password) {
+      // Have password
+      if(password.length < 6 || password.length >8 ){
+         console.log("400: Password complexity requirement not met:" + password);
+         res.writeHead(400,{'Content-Type' : 'application/json', 'Access-Control-Allow-Origin':'*'});
+         res.end(JSON.stringify({'message':'Password complexity requirement not met', 'developerMessage':'User creation failed because password complexity requirement not met'}));
+         return;
+      }else{
+         var reg = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[~!@#\$%\^&\*\+\=\`])(?=.{6,8})");
+         if(!reg.test(password)){
+            console.log("400: Password complexity requirement not met:" + password);
+            res.writeHead(400,{'Content-Type' : 'application/json', 'Access-Control-Allow-Origin':'*'});
+            res.end(JSON.stringify({'message':'Password complexity requirement not met', 'developerMessage':'User creation failed because password complexity requirement not met'}));
+            return;
+         }
+      }
+
+   // is user already exists
+   con.query("SELECT * FROM `app_users` WHERE `nic` LIKE '"+ nic + "'", function (err, result, fields) {
+      if (err) throw err;
+      console.log(result.length);
+
+      if(result.length>=1){
+         // user exists
+         res.writeHead(409,{'Content-Type' : 'application/json', 'Access-Control-Allow-Origin':'*'});
+         res.end(JSON.stringify({'message':'A user with nic: ' + nic + ' already exists', 'developerMessage':'User creation failed because the nic: ' + nic + ' already exists'}));
+         return;
+         
+      }else{
+         // User not exists
+
+         userId = getRandomArbitrary(10000,99999);
+         sql_newUser(userId, nic, mobile, pw);
+
+         // Need to check is user already exists
+         respData = {
+            'self': 'http://localhost:9090/api/users/' + userId,
+            'nic': nic,
+            'mobile':  mobile
+         }
+
+         res.writeHead(201,{'Content-Type' : 'application/json', 'Access-Control-Allow-Origin':'*'});
+         res.end(JSON.stringify(respData));
+         return;
+      }      
+
+   }else{
+     
+   }
+
+   // REM: hash the password
+
+});
+
+function getRandomArbitrary(min, max) {
+   return Math.floor(Math.random() * (max - min) + min);
+}
+
+function sql_newUser(userId, nic, mobile, password){
+   var query = con.query("INSERT INTO `app_users` (`id`, `name`, `nic`, `mobile`, `pw_hash`, `email` ) VALUES ('" + userId +"', 'user', '" + nic +"', '" + mobile +"', '', '');");
+
+   query.on('result', function(row) {
+      // Nothing to do
+      return true;
+   });
+}
+
+
+
 
 //------------------------------------------------------------------------------
 //-- MySQL support functions ---------------------------------------------------
